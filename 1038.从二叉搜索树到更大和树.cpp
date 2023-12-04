7a8e51822ef033f7b43d4aafa168bd0e188c088b
兮夜 <812288728@qq.com>
2023-12-04 22:57:24 +0800

    2023.12.4

---
+++ 1038.从二叉搜索树到更大和树.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=1038 lang=cpp
+ *
+ * [1038] 从二叉搜索树到更大和树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    int sum = 0;
+
+public:
+    TreeNode *bstToGst(TreeNode *root)
+    {
+        if (root)
+        {
+            bstToGst(root->right);
+            sum += root->val;
+            root->val = sum;
+            bstToGst(root->left);
+        }
+        return root;
+    }
+};
+// @lc code=end
--- test.cpp
+++ test.cpp
@@ -5,49 +5,14 @@
 using namespace std;
 
 void PrintVector(vector<int> &vec);
-
-// template <typename T>
-// class print
-// {
-// public:
-// 	void operator()(const T &elem)
-// 	{
-// 		cout << elem << ' ';
-// 	}
-// };
-
-template <class T>
-struct plus
-{
-	T operator()(const T &x, const T &y) const
-	{
-		return x + y;
-	}
-};
-
-template <class T>
-struct minus
-{
-	T operator()(const T &x, const T &y) const
-	{
-		return x - y;
-	}
-};
+string delSpaceStr(string s);
 
 int main()
 {
-	// int ia[6] = {0, 1, 2, 3, 4, 5};
-	// vector<int> iv(ia, ia + 6);
-	// for_each(iv.begin(), iv.end(), print<int>());
+	string s = "    aaabbb    ";
 
-	plus<int> plus_obj;
-	minus<int> minus_obj;
-
-	cout << plus_obj(3, 5) << endl;
-	cout << minus_obj(3, 5) << endl;
-
-	// cout << plus<int>()(1, 2) << endl;
-	// cout << minus<int>()(1, 2) << endl;
+	cout << delSpaceStr(s) << endl
+		 << delSpaceStr(s).length() << endl;
 
 	system("pause");
 	return 0;
@@ -58,5 +23,17 @@ void PrintVector(vector<int> &vec)
 	for (int &v : vec)
 		cout << v << " ";
 	cout << endl;
+}
+
+string delSpaceStr(string s)
+{
+	int n = s.length(), i = 0, j = n - 1;
+	while (i < n && s[i] == ' ')
+		i++;
+	while (j >= 0 && s[j] == ' ')
+		j--;
+	if (i <= j)
+		return s.substr(i, j - i + 1);
+	return "";
 }
 
