079d4bb59fab3e1616676cdc9e56069d4915fb92
兮夜 <812288728@qq.com>
2024-04-16 20:14:18 +0800

    2024.4.15

--- 131.分割回文串.cpp
+++ 131.分割回文串.cpp
@@ -10,40 +10,51 @@
 
 class Solution
 {
-private:
-    vector<vector<string>> ans;
-
 public:
     vector<vector<string>> partition(string s)
     {
-        int n = s.size();
+        int n = s.length();
+        vector<vector<string>> ans;
         vector<string> path;
-        backtracking(s, 0, 0, path);
+
+        function<void(int, int)> backtrack = [&](int level, int start) -> void
+        {
+            if (level == n)
+            {
+                ans.push_back(path);
+                return;
+            }
+            if (level < n - 1)
+            {
+                // 跳过当前字符 s[level]
+                backtrack(level + 1, start);
+            }
+
+            if (isPalindrome(s, start, level))
+            {
+                // s[start, level] 是一个回文串
+                path.push_back(s.substr(start, level - start + 1));
+                // 下一个回文串从 level+1 开始
+                backtrack(level + 1, level + 1);
+                // 回溯
+                path.pop_back();
+            }
+        };
+
+        backtrack(0, 0);
+
         return ans;
-    }
-    // 辅函数 - 回溯算法
-    void backtracking(const string &s, int level, int start, vector<string> path)
-    {
-        if (level == s.size())
-        {
-            ans.push_back(path);
-            return;
-        }
-        if (level < s.size() - 1)
-            backtracking(s, level + 1, start, path);
-        if (isPalindrome(s, start, level))
-        {
-            path.push_back(s.substr(start, level - start + 1));
-            backtracking(s, level + 1, level + 1, path);
-            path.pop_back();
-        }
     }
     // 辅函数 - 判断字符串 s 的 [left, right] 部分是否回文
     bool isPalindrome(const string &s, int left, int right)
     {
         while (left < right)
-            if (s[left++] != s[right--])
+        {
+            if (s[left] != s[right])
                 return false;
+            left++;
+            right--;
+        }
         return true;
     }
 };
